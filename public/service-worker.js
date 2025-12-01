6f98099f0a2254bc5dd2f4dbf996d6113884c491
Quang T <quangfr@users.noreply.github.com>
2025-12-01 13:11:42 +0100

    fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = '2025.11.30.23';
+const ASSET_VERSION = '2025.12.01';
 const CACHE_NAME = `habitube-app-${ASSET_VERSION}`;
 const ASSET_QUERY = `?v=${ASSET_VERSION}`;
 const OFFLINE_HTML = `/offline.html${ASSET_QUERY}`;
